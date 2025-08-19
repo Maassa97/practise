836f0e06c9ff298aeecdfcaf88b6da63f29ecf8f
AnnaKabanna <Anna.Lanigan@connect.qut.edu.au>
2025-08-19 23:36:02 +1000

    Fixed typo

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 	public static void Main(String[] args) {
-		System.out.println("Hello world!");
+		System.out.println("Hello, world!");
 		System.out.println("Welcome to CAB302!");
 	}
 }
